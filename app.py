df9e5e3f70f53d5fad09df5cc6ee16a451189ed3
Julien Fradin <julien@frad.in>
2025-07-09 13:46:26 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, make_response, request
+from flask_cors import CORS
+from generate_pdf import generate_pdf
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["POST"])
+def index():
+  body = request.json
+  content = generate_pdf(body['settings'], body['cards'])
+  resp = make_response(bytes(content), 201)
+  resp.headers.add('Content-type', 'application/pdf')
+  return resp
---
+++ generate_pdf.py
@@ -0,0 +1,67 @@
+from barcode import EAN13, Code128
+from barcode.writer import SVGWriter
+import qrcode
+from fpdf import FPDF, TextStyle, Align
+from fpdf.outline import TableOfContents
+import tempfile
+
+dpi=212
+
+def convert(cm):
+    return (cm / dpi) * 2.54
+
+ean13_width=convert(460)
+ean13_height=convert(200)
+qrcode_width=convert(300)
+
+document_width=convert(480)
+document_height=convert(800)
+
+def generate_pdf(settings, cards):
+    pdf = FPDF(format=(document_width, document_height), unit="cm")
+    pdf.set_margins(0.5, 0.5)
+    pdf.set_font("arial", size=12)
+    pdf.add_page()
+
+    if settings["includeTOC"]:
+        toc = TableOfContents()
+        pdf.insert_toc_placeholder(toc.render_toc, allow_extra_pages=False, pages=1)
+
+    first = True
+
+    for card in cards:
+        name = card["name"]
+        type = card["type"]
+        code = str(card["code"]).upper()
+        
+        if not first:
+            pdf.add_page()
+
+        first = False
+
+        pdf.start_section(name=name, level=0)
+
+        if settings["includeNames"]:
+            pdf.cell(text=name, w=(document_width - 1), align=Align.C)
+        # pdf.write_html("<h2>" + card["name"] + "</h2>" ,tag_styles=tag_styles)
+
+        if type == "EAN13" or type == "Code128":
+            with tempfile.NamedTemporaryFile(suffix=".svg", delete_on_close=False) as f:
+                if type == "EAN13":
+                    EAN13(code, writer=SVGWriter()).write(f)
+                elif type == "Code128":
+                    Code128(code, writer=SVGWriter()).write(f)
+                f.close()
+
+                pdf.image(f.name, w=ean13_width, h=ean13_height, y=((pdf.h - ean13_height) / 2), x=convert(10))
+            
+            pdf.set_y(5)
+        elif type == "QRCode":
+            image = qrcode.make(code)
+            pdf.image(image.get_image(), w=qrcode_width, h=qrcode_width, y=((pdf.h - qrcode_width) / 2), x=((pdf.w - qrcode_width) / 2))
+            pdf.set_y(6)
+
+        if settings["includeCodes"]:
+            pdf.cell(text=code, w=document_width - 1, h=1, align=Align.C)
+
+    return pdf.output()
